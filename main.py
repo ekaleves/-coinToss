a0242d36e16b9515268ef75f61cabb33f67648da
ekaleves <20237634-ekaleves@users.noreply.replit.com>
2023-04-10 11:12:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+
+print("Welcome to the virtual coin toss program.")
+coin = 0
+tossCoin = random.randint(1,2)
+if tossCoin == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+
+
+
+
+
+
+
+
+
